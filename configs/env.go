59bd4330065dd88c7f497629ec28782c2b748408
acy642 <kristoffer.schaaf@haw-hamburg.de>
2023-03-18 00:12:49 +0100

    build generic env mongoURI

--- configs/env.go
+++ configs/env.go
@@ -4,14 +4,30 @@ import (
 	"log"
 	"os"
 
+	"github.com/caarlos0/env/v6"
 	"github.com/joho/godotenv"
 )
+
+type Config struct {
+	MongoURIBeg string `env:"MONGOURI_BEG,required"`
+	MongoURIEnd string `env:"MONGOURI_END,required"`
+}
 
 func EnvMongoURI() string {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("unable to load .env file: %e", err)
 	}
 
-	return os.Getenv("MONGOURI")
+	cfg := Config{}
+
+	err = env.Parse(&cfg)
+	if err != nil {
+		log.Fatalf("unable to parse ennvironment variables: %e", err)
+	}
+
+	mongoDBPassword := os.Getenv("MONGO_DB_PASSWORD")
+	mongoURI := os.ExpandEnv(cfg.MongoURIBeg + mongoDBPassword + cfg.MongoURIEnd)
+
+	return mongoURI
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/caarlos0/env/v6 v6.10.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
