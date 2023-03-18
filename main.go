8c52e2b4bd2d2a0aa943e9c15ddb91f489ed002d
acy642 <kristoffer.schaaf@haw-hamburg.de>
2023-03-18 01:46:34 +0100

    add customer entity

--- main.go
+++ main.go
@@ -5,16 +5,25 @@ import (
 	"schuett-webapp-api/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowOrigins:     "http://localhost:4200",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 
 	//run database
 	configs.ConnectDB()
 
 	//routes
 	routes.UserRoute(app)
+	routes.CustomerRoute(app)
 
 	app.Listen(":8080")
 }
